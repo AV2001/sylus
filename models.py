e9aa4f653fb72610d503b51da9989cb7a1dc5b9a
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-22 22:41:21 +0400

    add register method on class that generates hashed password and returns user

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+
+bcrypt = Bcrypt()
 
 
 class User(db.Model):
@@ -12,3 +15,10 @@ class User(db.Model):
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
     password = db.Column(db.Text, nullable=False)
+
+    @classmethod
+    def register(cls, email, first_name, last_name, password):
+        '''Register user with hashed password and return the user'''
+        hashed_password = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed_password.decode('utf-8')
+        return cls(email=email, first_name=first_name, last_name=last_name, password=hashed_utf8)
