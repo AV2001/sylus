aad0743eb790d9f55c9c003db7bc4d664abd9c74
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-23 02:53:39 +0400

    add route to allow user to create an account

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
-from models import db
+from flask import Flask, flash, redirect, render_template
+from models import db, User
+from forms import RegisterUserForm
 
 # Load the enrivonment variables
 load_dotenv()
@@ -22,3 +23,20 @@ db.init_app(app)
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    '''Register a user.'''
+    form = RegisterUserForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        password = form.password.data
+        new_user = User.register(
+            first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
