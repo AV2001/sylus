3421464170b67aa67cf81b5e700650b1ebf9e51b
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-23 03:03:32 +0400

    add route to authenticate a logged in user

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, flash, redirect, render_template
+from flask import Flask, flash, redirect, render_template, session
 from models import db, User
-from forms import RegisterUserForm
+from forms import RegisterUserForm, LoginUserForm
 
 # Load the enrivonment variables
 load_dotenv()
@@ -40,3 +40,19 @@ def register_user():
         db.session.commit()
         return redirect('/login')
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def authenticate_user():
+    '''Authenticate a user.'''
+    form = LoginUserForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.authenticate(email=email, password=password)
+        if user:
+            session['email'] = user.email
+            return redirect('/')
+        else:
+            form.password.errors.append('Invalid email/password.')
+    return render_template('login.html', form=form)
