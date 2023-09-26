c4439ac624ccc5d0a73516b290ff8900be53eafd
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-26 13:37:26 +0400

    add icon column to Integration model

--- models.py
+++ models.py
@@ -54,6 +54,7 @@ class Integration(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=False, unique=True)
+    icon = db.Column(db.Text, nullable=False, unique=True)
 
 
 class UserIntegrations(db.Model):
