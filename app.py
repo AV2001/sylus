e21c413ac24f89aa327ce358ca410c8e9c40fc8d
AV2001 <anirudhvadlamani2001@gmail.com>
2023-10-01 19:39:58 +0400

    add outlook functionality

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, session, request
+from flask_migrate import Migrate
 from models import db, User, App, UserApps
 from forms import RegisterUserForm, LoginUserForm
+from O365.connection import Connection
+from completion import get_completion
+
+from O365 import Account
+
 
 # Load the environment variables
 load_dotenv()
@@ -17,6 +23,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 
 db.init_app(app)
+
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
@@ -70,7 +78,7 @@ def authenticate_user():
         user = User.authenticate(email=email, password=password)
         if user:
             session['email'] = user.email
-            return redirect('/chat')
+            return redirect('/dashboard')
         else:
             form.password.errors.append('Invalid email/password.')
     return render_template('login.html', form=form)
@@ -97,8 +105,16 @@ def add_app(app_id):
 @app.route('/chat')
 def show_chat_page():
     '''Show the chat page.'''
-    user = User.query.get(session['email'])
+    user = User.query.get(session.get('email'))
     return render_template('chat.html', user=user)
+
+
+@app.route('/chat', methods=['POST'])
+def process_chat_input():
+    prompt = request.form['prompt']
+    completion = get_completion(prompt)
+    print(completion)
+    return redirect('/chat')
 
 
 @app.route('/settings')
@@ -122,3 +138,54 @@ def delete_user():
 def logout_user():
     session.pop('email')
     return redirect('/')
+
+
+# OUTLOOK FUNCTIONALITY AND ROUTES
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+
+
+required_scopes = ['https://graph.microsoft.com/Mail.Read',
+                   'https://graph.microsoft.com/Mail.ReadWrite', 'https://graph.microsoft.com/Mail.Send']
+
+
+def get_authorization_url():
+    con = Connection((CLIENT_ID, CLIENT_SECRET), scopes=required_scopes)
+    consent_url, state = con.get_authorization_url(redirect_uri=REDIRECT_URI)
+
+    # Save the state in the session to validate later
+    session['oauth_state'] = state
+
+    return consent_url
+
+
+@app.route('/outlook-login')
+def login():
+    credentials = (CLIENT_ID, CLIENT_SECRET)
+    account = Account(credentials)
+    # redirect_uri = url_for('oauth_response', _external=True)
+    auth_url = account.authenticate(
+        scopes=required_scopes, redirect_uri=REDIRECT_URI)
+    if auth_url:
+        return redirect(auth_url)
+    else:
+        return 'Error generating authentication URL.', 500
+
+
+@app.route('/oauth-response')
+def oauth_response():
+    token_url = request.url
+
+    credentials = (CLIENT_ID, CLIENT_SECRET)
+    account = Account(credentials)
+    result = account.con.request_token(token_url, redirect_uri=REDIRECT_URI)
+
+    if result:
+        # Store the token in session or elsewhere securely
+        session["token"] = account.con.token_backend.token
+        flash('Outlook Authentication Successful', 'green')
+        return redirect('/chat')
+    else:
+        flash('Outlook Authentication Failed', 'red')
+        return redirect('/dashboard')
