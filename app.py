6983c96ae982cf4dbcb78da6789bc9ef4f1daafd
AV2001 <anirudhvadlamani2001@gmail.com>
2023-10-02 22:32:28 +0400

    change env variable name to DATABASE_URL

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 # CONFIGURATIONS
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 
